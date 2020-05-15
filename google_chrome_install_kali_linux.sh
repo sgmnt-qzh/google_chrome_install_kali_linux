2596fc37ed6b3060239b6900b74f236a2373be36
sgmnt-qzh <ipopravkin@gmail.com>
2020-05-15 17:39:49 +0300

    Create google_chrome_install_kali_linux.sh

---
+++ google_chrome_install_kali_linux.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+sudo apt-get update
+sudo apt-get install -y libappindicator1 fonts-liberation
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome*.deb
+google-chrome
